ad791e95def2eb4811e50043067458e8248f4d2e
KJM <KJM@KJM-PC>
2016-12-25 00:07:36 +0900

    initialize

---
+++ pga/src/main/java/com/sunandsea/pga/controller/MemberController.java
@@ -0,0 +1,13 @@
+package com.sunandsea.pga.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+	@RequestMapping("/list")
+	public String list() {
+		return "/WEB-INF/member/member.jsp";
+	}
+}
